4dff1eeada52613ac8f2b941645e4a4a83e184ca
Andrew <akorff@kentfieldschools.org>
2016-04-26 14:29:27 -0700

    Following the guidelines shown here: https://developer.chrome.com/extensions/background_pages as well as some content from the Event Page Example listed here: https://developer.chrome.com/extensions/samples , I've added a background.js script which now contains GetCount() and all its logic.

---
+++ background.js
@@ -0,0 +1,54 @@
+dateNow = new Date()
+dateFuture = new Date(2016,(dateNow.getMonth()),(dateNow.getDate()),15,28,40);
+delete dateNow
+//function goFullscreen(){chrome.windows.update(WindowType, {type: 'app'})} –Expand popup to fullscreen if possible…
+function GetCount(){
+
+        dateNow = new Date();                                                                        //grab current date
+        amount = dateFuture.getTime() - dateNow.getTime();                //calc milliseconds between dates
+        delete dateNow;
+
+        // time is already past
+        if(amount < 0){
+                out = "School's Out!";
+        }
+        // date is still good
+        else{
+                days=0;hours=0;mins=0;secs=0;out="";
+
+                amount = Math.floor(amount/1000);//kill the "milliseconds" so just secs
+
+                days=Math.floor(amount/86400);//days
+                amount=amount%86400;
+
+                hours=Math.floor(amount/3600);//hours
+                amount=amount%3600;
+
+                mins=Math.floor(amount/60);//minutes
+                amount=amount%60;
+
+                secs=Math.floor(amount);//seconds
+
+                if(days != 0){out += days +" day"+((days!=1)?"s":"")+", ";}
+                if(days != 0 || hours != 0){out += hours +" hour"+((hours!=1)?"s":"")+", ";}
+                if(days != 0 || hours != 0 || mins != 0){out += mins +" minute"+((mins!=1)?"s":"")+", ";}
+                out += secs +" seconds to dismissal";
+                
+                chrome.browserAction.setBadgeText({text: hours+":"+mins});
+        }
+        return out; // The variable "out" should be set to whatever value you want to appear on the popup page.
+}
+
+// Make sure this extension runs at browser startup.
+// https://developer.chrome.com/extensions/runtime#event-onStartup
+chrome.runtime.onStartup.addListener(function(){
+    GetCount();
+    setInterval(GetCount, 1000);
+})
+
+// Make sure this script runs when the extension is first installed (or reloaded after updating the code)
+// https://developer.chrome.com/extensions/runtime#event-onInstalled
+chrome.runtime.onInstalled.addListener(function(details) { // Details is just a placeholder and can be ignored.
+	GetCount();
+	setInterval(GetCount, 1000);
+})
--- popup.js
+++ popup.js
@@ -1,44 +1,10 @@
-dateNow = new Date()
-dateFuture = new Date(2016,(dateNow.getMonth()),(dateNow.getDate()),15,28,40);
-delete dateNow
-//function goFullscreen(){chrome.windows.update(WindowType, {type: 'app'})} –Expand popup to fullscreen if possible…
-function GetCount(){
-
-        dateNow = new Date();                                                                        //grab current date
-        amount = dateFuture.getTime() - dateNow.getTime();                //calc milliseconds between dates
-        delete dateNow;
-
-        // time is already past
-        if(amount < 0){
-                document.getElementById('countbox').innerHTML="School's Out!";
-        }
-        // date is still good
-        else{
-                days=0;hours=0;mins=0;secs=0;out="";
-
-                amount = Math.floor(amount/1000);//kill the "milliseconds" so just secs
-
-                days=Math.floor(amount/86400);//days
-                amount=amount%86400;
-
-                hours=Math.floor(amount/3600);//hours
-                amount=amount%3600;
-
-                mins=Math.floor(amount/60);//minutes
-                amount=amount%60;
-
-                secs=Math.floor(amount);//seconds
-
-                if(days != 0){out += days +" day"+((days!=1)?"s":"")+", ";}
-                if(days != 0 || hours != 0){out += hours +" hour"+((hours!=1)?"s":"")+", ";}
-                if(days != 0 || hours != 0 || mins != 0){out += mins +" minute"+((mins!=1)?"s":"")+", ";}
-                out += secs +" seconds to dismissal";
-                document.getElementById('countbox').innerHTML=out;
-                chrome.browserAction.setBadgeText({text: hours+":"+mins});
-        }
+function showCount() { // Call GetCount() from background.js, and use its return value to update the page.
+        var background = chrome.extension.getBackgroundPage();
+        document.getElementById('countbox').innerHTML = background.GetCount();
 }
 
+
 window.onload=function(){ //call when everything has loaded¸
-        GetCount(); // Call GetCount() once to initialize the display.
-        setInterval(GetCount, 1000); // Call GetCount() every second forever.
+        showCount(); // Call showCount() once to initialize the display.
+        setInterval(showCount, 1000); // Call showCount() every second forever.
 }
